a4804275f6abe71aad649fd44e128efc1b478c40
curbani <curbani@curbani-PC>
2015-06-23 18:33:39 -0300

    ytfuytbf

---
+++ src/br/furb/bcc/robotica/impressorabinaria/robo/config/RoboConfig.java
@@ -0,0 +1,14 @@
+package br.furb.bcc.robotica.impressorabinaria.robo.config;
+
+public class RoboConfig {
+	
+	
+public static final int DISTANCIA_CARINHO = 10;
+public static final int DISTANCIA_ESCRITOR = 10;
+public static final int DISTANCIA_CANETA = 10;
+public static final int VALOR_BAIXAR_CANETA = 35;
+public static final int VALOR_SUBIR_CANETA = 30;
+public static final int VALOR_ESCREVER_CANETA = 10;
+
+
+}
---
+++ src/br/furb/bcc/robotica/impressorabinaria/robo/movimentar/MovimentarRobo.java
@@ -0,0 +1,65 @@
+package br.furb.bcc.robotica.impressorabinaria.robo.movimentar;
+
+import java.util.ArrayList;
+
+import br.furb.bcc.robotica.impressorabinaria.modelo.Linha;
+import br.furb.bcc.robotica.impressorabinaria.modelo.Segmento;
+import br.furb.bcc.robotica.impressorabinaria.robo.config.RoboConfig;
+import lejos.nxt.Motor;
+
+public class MovimentarRobo {
+	private int posicaoEscritor;
+
+	public MovimentarRobo() {
+		this.posicaoEscritor = 0;
+	}
+
+	public void escreverLinha(Linha linha) {
+		ArrayList<Segmento> listSegmento = linha.getSegmentos();
+		movimentarCarinho();
+		for (Segmento unicoSegmento : listSegmento) {
+			escreverSegmento(unicoSegmento);
+		}
+		voltarCarinho();
+	}
+
+	public void escreverSegmento(Segmento segmento) {
+		while (posicaoEscritor < segmento.getFim()) {
+			if (posicaoEscritor < segmento.getInicio()) {
+				movimentarCaneta();
+				posicaoEscritor++;
+			} else {
+				baixarCaneta();
+				while (posicaoEscritor < segmento.getFim()) {
+					movimentarCaneta();
+					posicaoEscritor++;
+				}
+			}
+		}
+		levantarCaneta();
+	}
+
+	public void movimentarCarinho() {
+		Motor.A.rotate(RoboConfig.DISTANCIA_CARINHO);
+	}
+
+	public void movimentarCaneta() {
+		Motor.B.rotate(RoboConfig.DISTANCIA_CANETA);
+	}
+
+	public void voltarCarinho() {
+		while (posicaoEscritor > 0) {
+			Motor.B.rotate(RoboConfig.DISTANCIA_CANETA * (-1));
+			posicaoEscritor--;
+		}
+	}
+
+	public void levantarCaneta() {
+		Motor.C.rotate(RoboConfig.VALOR_SUBIR_CANETA);
+	}
+
+	public void baixarCaneta() {
+		Motor.C.rotate(RoboConfig.VALOR_BAIXAR_CANETA);
+	}
+
+}
