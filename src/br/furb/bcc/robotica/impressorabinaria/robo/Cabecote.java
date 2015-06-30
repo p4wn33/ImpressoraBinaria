798e09656c9c39d4317d6099c88178024d27797a
p4wn33 <p4wn33@201.54.204.19>
2015-06-30 18:44:49 -0300

    Cabecote

---
+++ src/br/furb/bcc/robotica/impressorabinaria/robo/Cabecote.java
@@ -0,0 +1,21 @@
+package br.furb.bcc.robotica.impressorabinaria.robo;
+
+public class Cabecote {
+	
+	private int nrLinha;
+	private int nrColuna;
+	
+	public int getNrLinha() {
+		return nrLinha;
+	}
+	public void setNrLinha(int nrLinha) {
+		this.nrLinha = nrLinha;
+	}
+	public int getNrColuna() {
+		return nrColuna;
+	}
+	public void setNrColuna(int nrColuna) {
+		this.nrColuna = nrColuna;
+	}
+
+}
