4830a46d3736189ef4ce4469d30bc222367ce3dc
ccurbani <ccurbani@MC-3507.furb.br>
2015-07-04 09:37:15 -0300

    teste flecha

--- src/br/furb/bcc/robotica/impressorabinaria/main/ExecutaTestes.java
+++ src/br/furb/bcc/robotica/impressorabinaria/main/ExecutaTestes.java
@@ -99,4 +99,84 @@ public class ExecutaTestes {
 		RoboImprime.getInstance().imprimirMatriz(GerenciadorMatriz.getInstance().getMatriz());
 		
 	}
+	
+	public static void testeManual3(){
+		
+		ArrayList<Segmento> listSegmento = new ArrayList();
+		Segmento segmento = new Segmento();
+		segmento.setInicio(5);
+		segmento.setFim(5);
+		listSegmento.add(segmento);			
+		Linha linha = new Linha(0,   listSegmento);
+	    //-------------------------------------	linha2---------------
+		ArrayList<Segmento> listSegmento2 = new ArrayList();
+		Segmento segmento2 = new Segmento();
+		segmento2.setInicio(4);
+		segmento2.setFim(6);
+		listSegmento2.add(segmento2);			
+		Linha linha2 = new Linha(1,   listSegmento2);
+		
+		//-------------------------------	
+		 //-------------------------------------	linha3---------------
+		ArrayList<Segmento> listSegmento3 = new ArrayList();
+		Segmento segmento3 = new Segmento();
+		segmento3.setInicio(3);
+		segmento3.setFim(7);
+		listSegmento3.add(segmento3);			
+		Linha linha3 = new Linha(2,   listSegmento3);
+		
+		//-------------------------------	
+		 //-------------------------------------	linha3---------------
+		ArrayList<Segmento> listSegmento4 = new ArrayList();
+		Segmento segmento4 = new Segmento();
+		segmento4.setInicio(2);
+		segmento4.setFim(8);
+		listSegmento4.add(segmento4);			
+		Linha linha4 = new Linha(3,   listSegmento4);
+		
+		//-------------------------------	
+		 //-------------------------------------	linha3---------------
+		ArrayList<Segmento> listSegmento5 = new ArrayList();
+		Segmento segmento5 = new Segmento();
+		segmento5.setInicio(4);
+		segmento5.setFim(6);
+		listSegmento5.add(segmento5);			
+		Linha linha5 = new Linha(4,   listSegmento5);
+		
+		//-------------------------------
+		 //-------------------------------------	linha3---------------
+		ArrayList<Segmento> listSegmento6 = new ArrayList();
+		Segmento segmento6 = new Segmento();
+		segmento6.setInicio(4);
+		segmento6.setFim(6);
+		listSegmento6.add(segmento6);			
+		Linha linha6 = new Linha(5,   listSegmento6);
+		
+		//-------------------------------
+		 //-------------------------------------	linha3---------------
+		ArrayList<Segmento> listSegmento7 = new ArrayList();
+		Segmento segmento7 = new Segmento();
+		segmento7.setInicio(4);
+		segmento7.setFim(6);
+		listSegmento7.add(segmento7);			
+		Linha linha7 = new Linha(6,   listSegmento7);
+		
+		//-------------------------------
+		
+		ArrayList<Linha> listLinhas = new ArrayList();
+		listLinhas.add(linha);
+		listLinhas.add(linha2);
+		listLinhas.add(linha3);
+		listLinhas.add(linha4);
+		listLinhas.add(linha5);
+		listLinhas.add(linha6);
+		listLinhas.add(linha7);
+		
+		Matriz matrizTeste  = new Matriz();
+		matrizTeste.setLinhas(listLinhas);
+		GerenciadorMatriz.getInstance().setMatriz(matrizTeste);
+		
+		RoboImprime.getInstance().imprimirMatriz(GerenciadorMatriz.getInstance().getMatriz());
+		
+	}
 }
